95d7ce8c424c7ceaaa650eae33ac56100a1e17e1
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-24 19:02:29 +0530

    second commit

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
+    username: String,
+
+    email: String,
+
+    password: String,
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    password: String,
+
+    otp: String,
+
+    otpExpiry: Date
 })
+
+const userModel = mongoose.model("user" , userSchema);
+
+export default userModel;
