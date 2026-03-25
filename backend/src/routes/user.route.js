b68ae005cd9b956122a4f85f1741306b395cf516
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-03-26 00:03:06 +0530

    authentication successfull

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { loginValidator, signupValidator } from "../validators/auth.validator.js";
+import {
+  loginValidator,
+  signupValidator,
+} from "../validators/auth.validator.js";
 import bcrypt from "bcryptjs";
 import validate from "../middlewares/validate.js";
 import User from "../models/user.model.js";
@@ -7,106 +10,114 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/signup" , signupValidator, validate, async (req,res) => {
-    const {email , username , password} = req.body;//iss way mein hum email username and password naam ki key ko access kar sakeingein
+router.post("/signup", signupValidator, validate, async (req, res) => {
+  const { email, username, password } = req.body; //iss way mein hum email username and password naam ki key ko access kar sakeingein
 
-    try{
-        const hashedPass = await bcrypt.hash(password,8);
-        const otp = Math.floor(Math.random()*9000 + 1000).toString()
-        const newUser = await User.create({
-            email,username,otp,
-            password: hashedPass,
-            otpExpiry: Date.now() + 5*60*1000
-        })
-        res.status(201).json({
-            message: "New user created successfully",
-            user: newUser
-        })
-    }catch(err){
-        res.json({error: err})
+  try {
+    const hashedPass = await bcrypt.hash(password, 8);
+    const otp = Math.floor(Math.random() * 9000 + 1000).toString();
+    const newUser = await User.create({
+      email,
+      username,
+      otp,
+      password: hashedPass,
+      otpExpiry: Date.now() + 5 * 60 * 1000,
+    });
+    res.status(201).json({
+      message: "New user created successfully",
+      user: newUser,
+    });
+  } catch (err) {
+    res.json({ error: err });
+  }
+});
+
+router.post("/otpVerification", async (req, res) => {
+  const { otp, email } = req.body;
+  const user = await User.findOne({
+    email: email,
+  });
+  if (!user) {
+    return res.status(400).json({
+      message: "user with this email not found",
+    });
+  }
+  const actualOTP = user.otp;
+  if (String(actualOTP) === String(otp)) {
+    if (Date.now() > user.otpExpiry) {
+      return res.status(401).json({
+        message: "OTP expired",
+      });
     }
 
-})
+    user.otp = undefined;
+    user.isVerified = true;
+    user.otpExpiry = undefined;
+    await user.save();
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
 
-router.post("/otpVerification" , async (req,res) => {
-    const {otp,email} = req.body;
-    const user = await User.findOne({
-        email: email
-    })
-    if(!user){
-        return res.status(400).json({
-            message: "user with this email not found"
-        })
-    }
-    const actualOTP = user.otp;
-    if(String(actualOTP) === String(otp)){
-        user.otp = undefined;
-        user.isVerified = true;
-        user.otpExpiry = undefined;
-        await user.save();
-        const token = jwt.sign({
-            id: user._id
-        },process.env.JWT_SECRET,{expiresIn: "1d"})
-        res.cookie("token",token,{
-            httpOnly: true
-        })
+    return res.status(201).json({
+      message: "User verified",
+    });
+  }
 
-        return res.status(201).json({
-            message: "User verified"
-        })
-    }
+  res.status(409).json({
+    message: "Invalid OTP...",
+  });
+});
 
-    res.status(409).json({
-        message : "Invalid OTP..."
-    })
+router.post("/login", loginValidator, validate, async (req, res) => {
+  const { identifier, password } = req.body;
 
-})
+  //checking what the heck is identifier
+  const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(identifier);
 
-router.post("/login" , loginValidator ,validate , async (req,res) => {
-    const {identifier , password} = req.body;
-    
-    //checking what the heck is identifier
-    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(identifier);
+  let user;
+  let isTrueUser;
 
-    let user;
-    let isTrueUser;
+  if (isEmail) {
+    user = await User.findOne({ email: identifier });
+  } 
+  else if (!isEmail) {
+    user = await User.findOne({ username: identifier });
+  }
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found...",
+    });
+  }
+  isTrueUser = await bcrypt.compare(password, user.password);
 
-    if(isEmail){
-       user = await User.findOne({email: identifier});
-       if(!user){
-        return res.status(409).json({
-            message: "User not found..."
-        })}
-        isTrueUser = await bcrypt.compare( password , user.password);       
-    }
-    else if(!isEmail){
-        user = await User.findOne({username: identifier});
-        if(!user){
-            return res.status(404).json({
-                message : "User not found..."
-            })
-        }
-        isTrueUser = await bcrypt.compare(password,user.password);
+  if (!isTrueUser) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
 
-    }
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
 
-    if(!isTrueUser){
-        return res.status(400).json({
-            message: "Invalid credentials"
-        })
-    }
+  res.cookie("token", token, {
+    httpOnly: true,
+  });
 
-    const token = jwt.sign({
-        id: user._id
-    },process.env.JWT_SECRET,{expiresIn: "1d"})
-
-    res.cookie("token",token,{
-        httpOnly: true
-    })
-
-    res.status(200).json({
-        message: "LoggedIN successfully"
-    })
-})
+  res.status(200).json({
+    message: "LoggedIN successfully",
+  });
+});
 
 export default router;
