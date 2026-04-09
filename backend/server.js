c1d80bc806fc45eece50b27f3bcc14926cf974c1
bhavyacoder2008 <bhavyamanochaviiilily@gmail.com>
2026-04-09 15:40:27 +0530

    gg

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 import connectDB from "./src/db/db.js";
 import genAIResponse from "./src/services/AI.js";
+import generatePDF from "./src/services/PDFGeneration.js";
 
-console.log(genAIResponse("Hello gemini"))
+// console.log(genAIResponse("Hello gemini"))
 
 
 dotenv.config();
 connectDB();
 
+await generatePDF()
 
-console.log(await genAIResponse("Hello gemini"))
+// console.log(await genAIResponse("Hello gemini"))
 
 app.listen(3000 , () => {
     console.log("Server started at PORT 3000")
--- backend/src/services/PDFGeneration.js
+++ backend/src/services/PDFGeneration.js
@@ -1,1 +1,37 @@
 import puppeteer from "puppeteer";
+
+const generatePDF = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setContent(`
+        
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>frontend</title>
+
+        <style>
+            h1{
+                color : red;
+            }
+        </style>
+        
+    </head>
+    <body class="bg-linear-to-br from-slate-900 via-purple-900 to-indigo-900">
+    <h1> Hello World </h1>
+    </body>
+    </html>
+
+        `, {waitUntil: ["load", "domcontentloaded", "networkidle0"]});
+
+    await page.pdf({
+        path: "output.pdf",
+        format: "A4",
+        printBackground: true
+    })
+  await browser.close();
+};
+
+export default generatePDF;
